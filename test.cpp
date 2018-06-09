01ed02eeb113032dfce02ae2b1234b46cf61d1b8
lotu <gao527950554@foxmail.com>
2018-06-09 11:51:39 +0800

    Task1测试代码完成

---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include<assert.h>
+#include "World.h"
+void TestInitWorld()
+{
+    int width=100;
+    int height=100;
+    bool ** map = new bool*[width];
+    for(int i=0;i<width;i++){
+        map[i] = new bool[height];
+    }
+    
+    World world(width,height,map);
+    assert(width == world.width());
+    assert(height == world.height());
+    for(int i=0;i<width;i++){
+        for(int j=0;j<height;j++)
+            assert(map[i][j] == world.map()[i][j]);
+    }
+}
+int main()
+{
+    TestInitWorld();
+
+    return 0;
+}
