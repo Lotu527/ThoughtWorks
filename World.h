e767e9ff39587d3d40f7f11fde5ed64c2a74db3d
lotu <gao527950554@foxmail.com>
2018-06-09 14:02:37 +0800

    TASK3：计算细胞下一状态实现代码

--- World.h
+++ World.h
@@ -45,6 +45,45 @@ public:
         show(_width,_height,_map);
     }
 
+    bool nextState(int cellsX, int cellsY){
+        
+        int livingCellsNum = 0;
+
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                if(cellsX-1+i < 0 || cellsY-1+j < 0)
+                    continue;
+                if(cellsX-1+i > _width-1 || cellsY-1+j > _height-1)
+                    continue;
+
+                if(i==1 && j==1)
+                    continue;
+                if(_map[cellsX-1+i][cellsY-1+j])
+                    livingCellsNum++;
+            }
+        }
+
+        if((livingCellsNum < 2)||(livingCellsNum > 3))
+            return 0;
+        if(livingCellsNum == 3)
+            return 1;
+        if(livingCellsNum == 2)
+            return _map[cellsX][cellsY];
+
+    }
+
+    bool **nextState(){
+        bool **mapNext = new bool*[_width];
+        for(int i=0;i<_width;i++){
+            mapNext[i] = new bool[_height];
+            for(int j=0; j< _height; j++){
+                mapNext[i][j] = nextState(i ,j );
+            }
+        }
+        return mapNext;
+    }
+
+
 private:
     int _width;
     int _height;
