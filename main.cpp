0ca70b76ca548552bc9ec01d45954751a11679dc
lotu <gao527950554@foxmail.com>
2018-06-10 12:27:36 +0800

    随机初始化世界方式更改

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "Display.h"
 #include "World.h"
-
+const int maxRow = 60;
+const int maxCow = 70;
+const int minRow = 10;
+const int minCow = 10;
 int main(int argc, char* argv[])
 {
     if(argc == 2){
@@ -8,9 +11,8 @@ int main(int argc, char* argv[])
         display.animation();
     }else if(argc == 1){
         srand(time(NULL));
-        Display display(new World(rand()%50+10,rand()%60+10));
+        Display display(new World(rand()%maxRow+minRow,rand()%maxCow+minCow));
         display.animation();
     }
-
     return 0;
 }
